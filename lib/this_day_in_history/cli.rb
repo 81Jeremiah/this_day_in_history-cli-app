9e188988ca7457fc10a50809b50a5d9c79fbfc02
81Jeremiah <jeremiahlutes@gmail.com>
2018-05-02 09:08:28 -0700

    added hardcoded functionality to CLI

--- lib/this_day_in_history/cli.rb
+++ lib/this_day_in_history/cli.rb
@@ -1,7 +1,34 @@
 class Cli
 
 	def call
-	 put" what would you like to learn about today?"
+		welcome
+	 puts "Welcome to the world of yesterday, today!"
+	 puts "Press 1 to list all"
+	 puts "Press 2 to see years"
+     puts "Press 3 to categories"
+
 	end
 
-end
+
+   def welcome
+   	  puts "Welcome to the world of yesterday, today!"
+	 puts "Press 1 to list all"
+	 puts "Press 2 to see years"
+     puts "Press 3 to categories"
+     puts "Type 'exit' to be down for the day"
+      input = gets.to_i
+
+      while input =! "exit"
+      	case input
+      	when 1
+      		history.all
+      		puts" mains story...."
+        when 2
+        	history.list_by_year
+            puts "in 1976...."
+            puts " 1998....."
+        when 3
+        	history.list_by_category
+     end
+
+  end
