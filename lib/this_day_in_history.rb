584247a538b715e9138252407c766ef223d7c2b4
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-05-03 20:42:15 -0700

    fixed executable file to require correct files for CLI

--- lib/this_day_in_history.rb
+++ lib/this_day_in_history.rb
@@ -1,4 +1,10 @@
-require "this_day_in_history/version"
+require 'open-uri'
+require 'pry'
+require 'nokogiri'
+
+require_relative "./this_day_in_history/version"
+require_relative "./this_day_in_history/cli"
+
 
 module ThisDayInHistory
   # Your code goes here...
