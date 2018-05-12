2390cc9a8643b03bc915efa22ca5675dc1873a2c
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-05-12 10:12:49 -0700

    fixed loop to allow exit
    input now is string so "exit" is recognized

--- lib/this_day_in_history/cli.rb
+++ lib/this_day_in_history/cli.rb
@@ -1,34 +1,38 @@
-class Cli
+require 'pry'
+class ThisDayInHistory::CLI
 
 	def call
 		welcome
-	 puts "Welcome to the world of yesterday, today!"
-	 puts "Press 1 to list all"
-	 puts "Press 2 to see years"
-     puts "Press 3 to categories"
-
 	end
 
 
    def welcome
-   	  puts "Welcome to the world of yesterday, today!"
-	 puts "Press 1 to list all"
-	 puts "Press 2 to see years"
+     input = ""
+     
+     while input != "exit"
+     puts "Welcome to the world of yesterday, today!"
+	   puts "Press 1 to list all"
+	   puts "Press 2 to see years"
      puts "Press 3 to categories"
-     puts "Type 'exit' to be down for the day"
-      input = gets.to_i
+     puts "Type 'exit' to be done for the day"
+     input = gets.strip
 
-      while input =! "exit"
+      
       	case input
-      	when 1
-      		history.all
+      	when "1"
+      		#history.all
       		puts" mains story...."
-        when 2
-        	history.list_by_year
-            puts "in 1976...."
-            puts " 1998....."
-        when 3
-        	history.list_by_category
-     end
+        when "2"
+        	#history.list_by_year
+          puts "in 1976...."
+          puts " 1998....."
+        when "3"
+          puts "Cold War"
+          puts "American"
 
-  end
+          #history.list_by_category
+      end
+
+    end
+end
+end
