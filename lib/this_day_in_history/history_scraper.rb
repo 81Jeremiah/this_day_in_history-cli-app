9aa8ec0cf8793a0b5feb063ebe841549a0c70a09
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-05-12 11:54:01 -0700

    added event_title local variable

--- lib/this_day_in_history/history_scraper.rb
+++ lib/this_day_in_history/history_scraper.rb
@@ -1,13 +1,25 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
+
 class History_Scraper
     def initialize
     end
 
 	def self.site_scraper
-		binding.pry
+		
 		doc = Nokogiri::HTML(open('https://www.history.com/this-day-in-history'))
 		
+		today = []
+		doc.css("div.day-article").each do |event|
+			event_title = event.css("title").text
+
+		end
+		binding.pry
+		event_title
 	end
 
 end
+
+History_Scraper.site_scraper
+#puts "is this working?"
