d2f043cb3cd8d2bd2c271b2b44a66722ddc196a5
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-05-12 15:44:24 -0700

    changed scraper to make events, added event class to make event objects

--- lib/this_day_in_history/history_scraper.rb
+++ lib/this_day_in_history/history_scraper.rb
@@ -3,16 +3,23 @@ require 'pry'
 require 'nokogiri'
 
 class History_Scraper
-    def initialize
-    end
+    # def initialize
+    # end
 
-	def self.site_scraper
+	def site_scraper
 		
 		doc = Nokogiri::HTML(open('https://www.history.com/this-day-in-history'))
+	end
 		
+     def get_events
+     	self.get_events.css(".day-articles")
+     end
 		today = []
-		doc.css("div.day-articles").each do |event|
+		doc.css("div.day-articles").collect do |event|
 			year = event.css(".year").text
+			event_title = event.css(."title")
+			category = event.css(".category")
+			story = event.css(".article").text
         today << {year: year}
 		end
 		binding.pry
